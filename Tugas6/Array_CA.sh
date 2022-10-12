3ad4e5e60388341ff6211538131ebc100224ff7b
Ilham zuhri <21083010112@student.upnjatim.ac.id>
2022-10-12 18:18:07 +0700

    Tugas 6

---
+++ Tugas6/Array_CA.sh
@@ -0,0 +1,4 @@
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
